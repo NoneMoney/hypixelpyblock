e10fce4d42586a75afb46b0f75bca0ce317d4833
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-09-20 07:32:11 +0000

    Edit index.html

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   id("org.springframework.boot") version "2.7.5"
-  id("io.spring.dependency-management") vers
+  id("io.spring.dependency-management") version "1.0.15.RELEASE"
+  kotlin("j
