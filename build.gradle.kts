fc246e62c87fdff5cf2faf8202fd5ee03cb46ba7
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-09-21 23:05:08 +0000

    added configuration file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,7 @@ plugins {
   id("io.spring.dependency-management") version "1.0.15.RELEASE"
   kotlin("jvm") version "1.7.20"
   kotlin("plugin.spring") version "1.7.20"
-  gr
+  groovy // for spock
+}
+
+group = "dev.yav
