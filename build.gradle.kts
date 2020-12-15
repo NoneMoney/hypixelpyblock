e16153baff883551b878f5de441ccf8b55dfe9de
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-12-15 08:19:25 +0000

    typo :)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,4 @@ repositories {
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
-  imp
+  implementation("org.jetbrains.kotl
