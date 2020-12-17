ac33df202a6efe2e890702eb9ae2dd5c6f8d771f
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-12-17 14:48:49 +0000

    Minor enhancement

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,4 +28,4 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementatio
+  implementation("io.github.micro
