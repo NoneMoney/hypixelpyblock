152c15cc867f3a091c9d21ee06914a0c1039c815
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-12-17 08:28:38 +0000

    minor code simplification

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,5 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlin:kotlin
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementatio
