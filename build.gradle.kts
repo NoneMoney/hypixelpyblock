9de4f02d28024a4d5b8be2dd0f1b3f547f371848
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2020-12-17 22:03:46 +0000

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,4 +28,5 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-  implementation("io.github.micro
+  implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+ 
