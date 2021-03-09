d5fa97edb2e7f17a63fbf1b5a333d8a227c8809c
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-03-09 20:51:39 +0000

    edited comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,4 +36,5 @@ dependencies {
 
   runtimeOnly("io.micrometer:micrometer-registry-prometheus")
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-  testImplementation("org.springframework.boot:spring-boot-starter-test"
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("or
