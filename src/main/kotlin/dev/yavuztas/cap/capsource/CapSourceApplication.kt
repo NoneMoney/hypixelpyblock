d9ac7beb72a0f2517a754ee83927088d2a2b579b
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-06-22 07:49:01 +0000

    Update Roadmap

---
+++ src/main/kotlin/dev/yavuztas/cap/capsource/CapSourceApplication.kt
@@ -0,0 +1,8 @@
+
+package dev.yavuztas.cap.capsource
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
