24000b8c1137fcaae2376c511ad75750320b2fe0
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-06-30 09:08:11 +0000

    migrate

--- src/main/kotlin/dev/yavuztas/cap/capsource/CapSourceApplication.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/CapSourceApplication.kt
@@ -6,3 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
+class CapSourceApplication
+
+fun main(args: Array<String>) {
+  runApplication<CapSourceApplication>(*args)
+}
---
+++ src/main/kotlin/dev/yavuztas/cap/capsource/config/AppConfiguration.kt
@@ -0,0 +1,26 @@
+
+package dev.yavuztas.cap.capsource.config
+
+import dev.yavuztas.cap.capsource.registry.Registry
+import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
+import dev.yavuztas.cap.capsource.config.properties.VertxProperties
+import dev.yavuztas.cap.capsource.feed.FeedSupplier
+import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class AppConfiguration(
+  private val vertxProps: VertxProperties,
+  private val registryProps: RegistryProperties
+) {
+
+  @Bean
+  fun vertx(): Vertx {
+    return Vertx.vertx(
+      VertxOptions()
+        .setEventLoopPoolSize(vertxProps.eventLoopPoolSize)
+    )
+  }
