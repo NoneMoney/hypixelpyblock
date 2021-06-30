beec58662a778dec14f4fd94e97889bcaf5473b9
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-06-30 19:36:24 +0000

    Copy edit

--- src/main/kotlin/dev/yavuztas/cap/capsource/config/properties/VertxProperties.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/config/properties/VertxProperties.kt
@@ -3,3 +3,9 @@ package dev.yavuztas.cap.capsource.config.properties
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "app.vertx")
+data class VertxProperties(
+  val eventLoopPoolSize: Int
+)
---
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/FeedConsumer.kt
@@ -0,0 +1,12 @@
+
+package dev.yavuztas.cap.capsource.feed
+
+interface FeedConsumer {
+
+  /**
+   * Implementors should implement this method in a non-blocking way
+   * Otherwise, it may block the supplier.
+   */
+  fun consume(supplier: FeedSupplier)
+
+}
