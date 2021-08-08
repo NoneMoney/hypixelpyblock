65abe06012cd6c55b6e330ba6dd029703b154572
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-08-08 07:17:38 +0000

    fixed spacing issue

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/FeedDataStream.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/FeedDataStream.kt
@@ -3,4 +3,6 @@ package dev.yavuztas.cap.capsource.feed
 import io.vertx.core.Handler
 import io.vertx.core.buffer.Buffer
 import io.vertx.core.streams.ReadStream
-import java.util.concurrent.atomic
+import java.util.concurrent.atomic.AtomicLong
+
+class FeedDataStream
