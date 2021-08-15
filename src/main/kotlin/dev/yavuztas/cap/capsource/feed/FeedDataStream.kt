34c7f065c843907bb6a6e949ef469256f5686790
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-08-15 16:17:19 +0000

    version bump to 0.3.2

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/FeedDataStream.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/FeedDataStream.kt
@@ -14,4 +14,8 @@ class FeedDataStream (private val supplier: FeedSupplier) : ReadStream<Buffer> {
   private var eventHandler: Handler<Buffer>? = null
 
   override fun exceptionHandler(handler: Handler<Throwable>?): ReadStream<Buffer> {
-    this.exceptionHandler = h
+    this.exceptionHandler = handler
+    return this
+  }
+
+  ove
