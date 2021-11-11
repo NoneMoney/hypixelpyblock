46c4c9840c2d80bc0fbc8fd51eaee8470809c714
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-11-11 19:48:24 +0000

    Updated the awswrangler feedstock.

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
@@ -4,4 +4,4 @@ import com.binance.connector.client.WebsocketClient
 import com.binance.connector.client.impl.WebsocketClientImpl
 import com.binance.connector.client.utils.WebSocketCallback
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
-import d
+import dev.yavuztas.cap.capsource.feed
