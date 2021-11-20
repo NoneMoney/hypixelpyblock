a71ac63b1457f752bb39500b9dcdc2b150235e11
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-11-20 13:15:47 +0000

    Updated handbook.

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
@@ -5,4 +5,5 @@ import com.binance.connector.client.impl.WebsocketClientImpl
 import com.binance.connector.client.utils.WebSocketCallback
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedData
-import dev.yavuztas.cap.c
+import dev.yavuztas.cap.capsource.feed.RawFeedData
+import 
