ea2bb27e7f76b7c07bba67868cd5e541730d8842
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-11-20 18:17:52 +0000

    Apply coding standard fixes

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
@@ -6,4 +6,5 @@ import com.binance.connector.client.utils.WebSocketCallback
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedData
 import dev.yavuztas.cap.capsource.feed.RawFeedData
-import dev.yavuztas.cap.capsource.fe
+import dev.yavuztas.cap.capsource.feed.FeedSupplier
+import dev.yavuztas.cap.c
