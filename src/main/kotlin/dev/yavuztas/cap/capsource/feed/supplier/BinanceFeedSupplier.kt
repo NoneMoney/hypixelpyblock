dad3f8955d9569ce6aefe57ca20dd76bebc7969c
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2021-11-27 10:41:49 +0000

    ARM: move VFP init to an earlier boot stage

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
@@ -9,4 +9,5 @@ import dev.yavuztas.cap.capsource.feed.RawFeedData
 import dev.yavuztas.cap.capsource.feed.FeedSupplier
 import dev.yavuztas.cap.capsource.util.RingBuffer
 import mu.KotlinLogging
-import org.springframework.bea
+import org.springframework.beans.factory.annotation.Value
+import
