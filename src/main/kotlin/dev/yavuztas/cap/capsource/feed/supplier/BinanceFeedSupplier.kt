d6a0e94677dbb634e4ec8b111d151ed26b922b42
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-02-20 13:50:29 +0000

    added release date

--- src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/feed/supplier/BinanceFeedSupplier.kt
@@ -50,4 +50,4 @@ class BinanceFeedSupplier(
     log.debug { "supplied: ${message}, write index: ${buffer.writeIndex()}" }
     // trigger consumers, currently binance's @miniTicker pushes every second only once.
     // So, we don't need debouncing here
-   
+    // TODO consider debouncing if the so
