da47e1315fb15c9e150b9430f220310be9050885
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-03-29 13:18:33 +0000

    Minor style tweaks

--- src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
@@ -1,4 +1,5 @@
 package dev.yavuztas.cap.capsource.registry
 
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
-import dev.yavuztas.cap.capso
+import dev.yavuztas.cap.capsource.feed.FeedConsumer
+import dev.yav
