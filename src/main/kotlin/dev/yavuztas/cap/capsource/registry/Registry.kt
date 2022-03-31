e59c3fc717d95b224a94b7f80ffe0114d18f28af
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-03-31 16:01:20 +0000

    update some deps

--- src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
@@ -3,4 +3,5 @@ package dev.yavuztas.cap.capsource.registry
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedSupplier
-import io.vertx.core.Abstract
+import io.vertx.core.AbstractVerticle
+import io.vertx.c
