e1615791e2e62ed85879669b9998d5b1fde9d529
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-04-01 18:38:39 +0000

    Updated the dials-data feedstock.

--- src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/registry/Registry.kt
@@ -4,4 +4,5 @@ import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedSupplier
 import io.vertx.core.AbstractVerticle
-import io.vertx.c
+import io.vertx.core.DeploymentOptions
+import i
