28026a47e5d12e5980baa814830e52f3abc20bfc
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-08-05 06:39:26 +0000

    dev build

--- src/main/kotlin/dev/yavuztas/cap/capsource/registry/RegistryClient.kt
+++ src/main/kotlin/dev/yavuztas/cap/capsource/registry/RegistryClient.kt
@@ -8,4 +8,4 @@ import mu.KotlinLogging
 class RegistryClient (private val socket: NetSocket) {
 
   private val log = KotlinLogging.logger {}
-  private val streams: MutableLis
+  private val streams: MutableList<FeedDataStream> = ArrayList()
