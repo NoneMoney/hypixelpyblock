205752ade9b336bde293bb9b5765e648839c36b3
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-08-30 22:15:50 +0000

    Promoting a new nightly build for commons

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -2,4 +2,4 @@ package dev.yavuztas.cap.capsource.registry
 
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
-import dev.ya
+import dev.yavuztas.cap.capsource.
