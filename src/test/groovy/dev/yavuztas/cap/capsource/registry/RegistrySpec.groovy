ab408c7f7625cab95e767c6335470c36be4b6a7f
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-08-30 17:19:44 +0000

    random: Add comment to random_initialize()

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -1,4 +1,5 @@
 package dev.yavuztas.cap.capsource.registry
 
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
-import dev.yavuztas.cap.ca
+import dev.yavuztas.cap.capsource.feed.FeedConsumer
+import dev.ya
