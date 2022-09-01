f21abdd31c51fd4118f3234218e65fc9d2be68a9
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-09-01 22:07:17 +0000

    install scripts

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -3,4 +3,4 @@ package dev.yavuztas.cap.capsource.registry
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedData
-import d
+import dev.yavuztas.cap.capsource.feed.Fee
