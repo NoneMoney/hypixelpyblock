1eefa843e8c2db5354ee614bbb4641c8cdc8b83e
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-09-01 07:03:39 +0000

    update version numbers and release notes for release

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -2,4 +2,5 @@ package dev.yavuztas.cap.capsource.registry
 
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
-import dev.yavuztas.cap.capsource.
+import dev.yavuztas.cap.capsource.feed.FeedData
+import d
