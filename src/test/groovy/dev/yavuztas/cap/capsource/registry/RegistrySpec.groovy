e5022f7e228860412b8fcffba1eca298189d13ca
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-09-02 05:55:01 +0000

    Fix autocomplete

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -3,4 +3,5 @@ package dev.yavuztas.cap.capsource.registry
 import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedData
-import dev.yavuztas.cap.capsource.feed.Fee
+import dev.yavuztas.cap.capsource.feed.FeedSupplier
+import dev.yavuztas.
