2c3445987d71c3f5b7f6c4c95e9edb184732a06c
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-09-02 17:16:41 +0000

    Modified   ManaPHP/Db.php

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -4,4 +4,4 @@ import dev.yavuztas.cap.capsource.config.properties.RegistryProperties
 import dev.yavuztas.cap.capsource.feed.FeedConsumer
 import dev.yavuztas.cap.capsource.feed.FeedData
 import dev.yavuztas.cap.capsource.feed.FeedSupplier
-import dev.yavuztas.
+import dev.yavuztas.cap.capsource.feed.RawFeedDa
