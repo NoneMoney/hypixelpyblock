9082c8b2f634b811ea07c9bf379d589fbe18e4bb
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-09-25 16:41:16 +0000

    Fix spelling typo

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -17,4 +17,5 @@ import spock.util.concurrent.AsyncConditions
 import java.nio.charset.StandardCharsets
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
-import ja
+import java.util.concurrent.TimeUnit
+import j
