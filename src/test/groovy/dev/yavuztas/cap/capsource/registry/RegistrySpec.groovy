c1e01dce5656adc4630fc90fd4bd55506d36cbaa
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2022-11-26 16:43:26 +0000

    Create tags.html

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -59,4 +59,4 @@ class RegistrySpec extends Specification {
 
       @Override
       long forEach(long readIndex, long amount, @NotNull Consumer<FeedData> action) {
-       
+        long to = Math.min(writeIn
