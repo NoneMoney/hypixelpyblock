e1771e2080fe0fb805c6cd465118ff23f859a758
NoneMoney <105919663+NoneMoney@users.noreply.github.com>
2023-03-22 17:27:39 +0000

    add instagram

--- src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
+++ src/test/groovy/dev/yavuztas/cap/capsource/registry/RegistrySpec.groovy
@@ -165,4 +165,5 @@ class RegistrySpec extends Specification {
     def ac = new AsyncConditions(5)
     def client = createClient((s,m) -> {
       Thread.sleep(1000)
-      log.info("client#${s.localAddress()} readIndex: ${clientReadIndex.incrementAndGet()}, messag
+      log.info("client#${s.localAddress()} readIndex: ${clientReadIndex.incrementAndGet()}, message: ${m}")
+      ac.evaluate { asser
